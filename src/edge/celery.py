f188bbe0e2a96683beb081e39a5619c34aac09d7
mrin247 <mrinmoym247@gmail.com>
2025-03-29 21:21:22 +0530

    Integrated celery beat scheduler

--- src/edge/celery.py
+++ src/edge/celery.py
@@ -1,8 +1,21 @@
 import os
 from celery import Celery
 
+# import eventlet
+# eventlet.monkey_patch()
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edge.settings")
+
 
 app = Celery("edge")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+CELERY_BEAT_SCHEDULE = {
+    "check-temp": {
+        "task": "sensors.tasks.measure_temp_task",
+        "schedule": 5,
+    },
+}
+
+app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
