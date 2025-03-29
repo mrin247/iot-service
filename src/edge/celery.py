91b0c6e74e72aa75654413abc5262dc6b6338dc8
mrin247 <mrinmoym247@gmail.com>
2025-03-30 00:48:57 +0530

    Containerized the application

--- src/edge/celery.py
+++ src/edge/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from kombu import Queue, Exchange
 
 # import eventlet
 # eventlet.monkey_patch()
@@ -11,11 +12,22 @@ app = Celery("edge")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
-CELERY_BEAT_SCHEDULE = {
-    "check-temp": {
+nodes = [
+    "node-1",
+    "node-2",
+    "node-3"
+]
+
+CELERY_TASK_QUEUES = []
+CELERY_BEAT_SCHEDULE = {}
+for node in nodes:
+    key = f"check-temp-{node}"
+    CELERY_TASK_QUEUES.append(Queue(node, Exchange(node), routing_key=node))
+    CELERY_BEAT_SCHEDULE[key] = {
         "task": "sensors.tasks.measure_temp_task",
         "schedule": 5,
-    },
-}
-
+        "options": {"queue": node}, # Apply async
+    }
+    
+app.conf.task_queues = CELERY_TASK_QUEUES
 app.conf.beat_schedule = CELERY_BEAT_SCHEDULE
