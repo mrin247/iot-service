c107dce8356b23a23aa5d5fb909b7802b07f5d79
mrin247 <mrinmoym247@gmail.com>
2025-03-29 14:24:41 +0530

    Integrated celery with redis broker

--- src/edge/settings.py
+++ src/edge/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_celery_beat",
+    "django_celery_results",
     
     "sensors",
 ]
@@ -137,3 +139,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Celery
+CELERY_BROKER_URL = config("CELERY_BROKER_REDIS_URL", cast=str, default="redis://localhost:6379")
+CELERY_RESULT_BACKEND = "django-db"
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_REDIS_BACKEND_USE_SSL = False
+CELERY_BROKER_USE_SSL = False
